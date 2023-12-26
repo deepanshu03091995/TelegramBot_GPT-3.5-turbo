d4067e3ed691808c7eecbde879d3602cd9d6b96d
Deepanshu03091995 <43910264+deepanshu03091995@users.noreply.github.com>
2023-12-26 15:57:29 +0530

    updated telegram_bot.py

---
+++ Research/telegram_bot.py
@@ -0,0 +1,25 @@
+import logging
+from aiogram import Bot, Dispatcher, types,executor
+from dotenv import load_dotenv
+
+import os
+load_dotenv()
+
+
+API_TOKEN=os.environ["TOKEN"]
+
+logging.basicConfig(level=logging.INFO)
+bot=Bot(token=API_TOKEN)
+dp=Dispatcher(bot)
+
+@dp.message_handler(commands=['start','help'])
+async def command_start_handler(message: types.Message):
+    """
+    This handler receives messages with `/start` ar `/help` command
+    """
+    
+    await message.reply("Hi\nI am EchoBot\n Powered by aiogram")
+    
+if __name__=="__main__":
+    executor.start_polling(dp,skip_updates=True)    
+    
